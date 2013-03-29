78336cfc05d00b73211b9b7c6ff00afffef9a18b
jamesuhe <jamesuhe@gmail.com>
2013-03-28 21:28:51 -0400

    Buttons are somewhat implemented.

--- src/shrider/uhe/graphics/GLRenderer.java
+++ src/shrider/uhe/graphics/GLRenderer.java
@@ -10,7 +10,9 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
+import android.view.View;
 
 public class GLRenderer implements Renderer {
 
@@ -19,51 +21,56 @@ public class GLRenderer implements Renderer {
 	private float[] light0Pos = {0f, 0f, 0f, 1f};
 	private float[] matAmb = {.3f, .3f, .3f, 1f};
 	private float[] matSpec = {.7f, 1f, .7f, 1f};
-	
+
 	private float[] transRot;
 	private float[] transTran;
 	private float[] transScale;
-	
+
 	private float[] cfWorld;
-	
+
 	//short amount=8;
-//	short faces=3;
+	//	short faces=3;
 	private FloatBuffer bufVer, bufNorm, bufCol;
 	private ShortBuffer bufDraw;
 	ByteBuffer buff;
 	private float[] arrVer = {	.5f,-.5f,0f, 	//0
-								.5f,.5f,0f,	//1
-								-.5f,.5f,0f,	//2
-								-.5f,-.5f,0f,		//3
-								-.5f,.5f,-1f,	//4
-								-.5f,-.5f,-1f,	//5
-								-.6f,-.6f,-1f,	//6
-								-.5f,-.57f,-1f		//7
-							 };
+			.5f,.5f,0f,	//1
+			-.5f,.5f,0f,	//2
+			-.5f,-.5f,0f,		//3
+			-.5f,.5f,-1f,	//4
+			-.5f,-.5f,-1f,	//5
+			-.6f,-.6f,-1f,	//6
+			-.5f,-.57f,-1f		//7
+	};
 	private short[] arrDraw = {	0,2,1,
-								0,1,3,
-								7,5,6,
-								7,5,4
-							  };
+			0,1,3,
+			7,5,6,
+			7,5,4
+	};
 	private float[] arrNorm = {	-1f,1f,1f,
-								-1f,-1f,1f,
-								1f,-1f,1f,
-								1f,1f,1f,
-								-1f,1f,-1f,
-								-1f,-1f,-1f,
-								1f,-1f,-1f,
-								1f,1f,-1f
-								};
+			-1f,-1f,1f,
+			1f,-1f,1f,
+			1f,1f,1f,
+			-1f,1f,-1f,
+			-1f,-1f,-1f,
+			1f,-1f,-1f,
+			1f,1f,-1f
+	};
 	private float[] arrCol = {	1f,0,0,1f,
-								0,1f,0,1f,
-								0,0,1f,1f,
-								1f,1f,0,1f,
-								1f,0,1f,1f,
-								0f,1f,1f,1f,
-								0f,0f,0f,1f,
-								.4f,1f,1f,1f
-							 };
-	
+			0,1f,0,1f,
+			0,0,1f,1f,
+			1f,1f,0,1f,
+			1f,0,1f,1f,
+			0f,1f,1f,1f,
+			0f,0f,0f,1f,
+			.4f,1f,1f,1f
+	};
+	private float[] lookAt={
+			 0, 0, 8,
+				0, 0, 0, 
+				0, 0, 1f
+	};
+
 	boolean isAnimating;
 	Context mCtx;
 
@@ -75,46 +82,46 @@ public class GLRenderer implements Renderer {
 		transTran = new float[16];
 		transScale = new float[16];
 		cfWorld = new float[16];
-		
+
 		for (int i=0;i<16;i+=5){ //everything in here becomes I
 			cfWorld[i] = 1f;
-			transRot[i] = 1f;
-			transTran[i] = 1f;
-			transScale[i] = 1f;
+		transRot[i] = 1f;
+		transTran[i] = 1f;
+		transScale[i] = 1f;
 		}
-			
+
 	}
 	@Override
 	public synchronized void onDrawFrame(GL10 gl) {
 		gl.glClearColor(1f, .2f, .2f, 1f);
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 		gl.glLoadIdentity();
-		
-		
-		GLU.gluLookAt(gl, 0, 0, 8,
-				0, 0, 0, 
-				0, 0, 1f); //take a step back...or 8
+
+
+//		GLU.gluLookAt(gl, lookAt[0], lookAt[1], lookAt[2],
+//				lookAt[2], lookAt[3], lookAt[4], 
+//				lookAt[5], lookAt[6], lookAt[7]); //take a step back...or 8
 		gl.glColor4f(1.0f,1.0f,1.0f,1.0f);
-		cfWorld[4] = .4f;
+//		cfWorld[4] = .4f;
 		gl.glMultMatrixf(cfWorld, 0);
-		
+
 		//begin the actual render
 		gl.glPushMatrix();
 		gl.glTranslatef(.3f, .4f, -.5f);
 		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, light0Pos, 0);
 		gl.glPopMatrix();
-		
+
 		gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
-		
+
 		gl.glPushMatrix();
 		gl.glVertexPointer(3,GL10.GL_FLOAT,0,bufVer);
 		gl.glColorPointer(4,GL10.GL_FLOAT,0,bufCol);
 		gl.glNormalPointer(GL10.GL_FLOAT, 0, bufNorm);
-		
+
 		gl.glDrawElements(GL10.GL_TRIANGLES, arrDraw.length, GL10.GL_UNSIGNED_SHORT, bufDraw);
-		
+
 		gl.glPopMatrix();
-		
+
 	}
 
 	@Override
@@ -123,7 +130,7 @@ public class GLRenderer implements Renderer {
 		gl.glViewport(0, 0, width, height);
 		gl.glMatrixMode(GL10.GL_PROJECTION);
 		gl.glLoadIdentity();
-		
+
 		boolean isLandscape = width > height;
 		float aspect = (float) width / height;
 		GLU.gluPerspective(gl, 60, aspect, .1f, 20.0f);
@@ -135,24 +142,30 @@ public class GLRenderer implements Renderer {
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 		gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
 		gl.glShadeModel(GL10.GL_SMOOTH);
-		
+
 		gl.glEnable(GL10.GL_LIGHTING);
 		gl.glEnable(GL10.GL_LIGHT0);
 		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_AMBIENT, light0Amb, 0);
 		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, light0Dif, 0);
 		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, light0Pos, 0);
-		
+
 		gl.glLightModelfv(GL10.GL_LIGHT_MODEL_AMBIENT, light0Amb, 0);
 		gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_AMBIENT_AND_DIFFUSE, matAmb,0);
 		gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SPECULAR, matSpec, 0);
 		gl.glMaterialf(GL10.GL_FRONT_AND_BACK, GL10.GL_SHININESS, 10); //only if want shiny
 		gl.glEnable(GL10.GL_COLOR_MATERIAL);
-		
+
 		gl.glEnable(GL10.GL_CULL_FACE);
 		gl.glEnable(GL10.GL_DEPTH_TEST);
-		
+
+		gl.glClearColor(1f, .2f, .2f, 1f);
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity();
+		GLU.gluLookAt(gl, lookAt[0], lookAt[1], lookAt[2],
+				lookAt[2], lookAt[3], lookAt[4], 
+				lookAt[5], lookAt[6], lookAt[7]); //take a step back...or 8
 		initArrays();
-		
+
 	}
 
 	private void initArrays(){
@@ -168,16 +181,72 @@ public class GLRenderer implements Renderer {
 		buff = ByteBuffer.allocateDirect(arrDraw.length * 2);
 		buff.order(ByteOrder.nativeOrder());
 		bufDraw = buff.asShortBuffer();
-		
-		
+
+
 		bufVer.put(arrVer);
 		bufCol.put(arrCol);
 		bufDraw.put(arrDraw);
 		bufNorm.put(arrNorm);
-        
+
 		bufVer.position(0);
 		bufCol.position(0);
 		bufNorm.position(0);
 		bufDraw.position(0);
+	}
+
+	public void Click(char action,View v){
+		switch(action){
+	     case '0':         //Forward
+             
+	    	 cfWorld[1]=cfWorld[1]+.1f;
+
+             break;
+     case '1':         //Backward
+
+    	 cfWorld[1]=cfWorld[1]-.5f;
+             break;
+     case '2':         //left
+
+    	 cfWorld[2]=cfWorld[3]+.5f;
+             break;
+     case '3':         //right
+
+    	 cfWorld[2]=cfWorld[2]-.1f;
+             break;
+     case '4':         //turn left
+             break;
+     case '5':         //turn right
+         break;
+ case '6':         //tilt up
+         break;
+ case '7':         //tilt down
+         break;
+		
+/*		
+		
+		
+		case 0:					//Up
+				System.out.println("UP");
+			break;
+		case '1':					//Down
+			MoveEye(0);
+			break;
+		case '2':					//Right
+				MoveEye(move)
+			break;
+		case '3':					//Left
+
+			break;
+		case '4':					//rotate
+
+			break;
+
+*/		}
+		
+		return;
+	}
+	
+	public void MoveEye(int move){
+		
 	}
 }
